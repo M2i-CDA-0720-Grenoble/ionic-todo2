11fc51d371df578048c0fe6cb6cfdcdeb0e3d268
Romain Tello <romain.tello@gmail.com>
2021-01-06 11:47:04 +0100

    Add new todo creation

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,52 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import React from 'react';
+import { IonButton, IonContent, IonHeader, IonInput, IonItem, IonLabel, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import React, { FC, useState } from 'react';
 import TodoList from '../components/TodoList';
 import { TodoModel } from '../models';
 import './Home.css';
 
+const defaultTodos: TodoModel[] = [
+  { text: 'Bananes', done: false },
+  { text: 'Chocolat', done: false },
+  { text: 'Poires', done: false },
+];
+
+interface AddTodoFormProps {
+  addTodo: (newTodo: TodoModel) => void,
+}
+
+const AddTodoForm: FC<AddTodoFormProps> = ({ addTodo }) => {
+  const [text, setText] = useState('');
+
+  const handleClick = () => {
+    addTodo({
+      text,
+      done: false,
+    });
+  }
+
+  return (
+    <>
+      <IonItem>
+        <IonLabel position="floating">Nouvelle tâche</IonLabel>
+        <IonInput
+          value={text}
+          onIonChange={(event) => setText( (event.target as HTMLInputElement).value)}
+        />
+      </IonItem>
+      <IonButton onClick={handleClick} expand="block">Ajouter</IonButton>
+    </>
+  );
+}
+
 const Home: React.FC = () => {
-  const todos: TodoModel[] = [
-    { text: 'Bananes', done: false },
-    { text: 'Chocolat', done: false },
-    { text: 'Poires', done: false },
-  ];
+  const [todos, setTodos] = useState<TodoModel[]>(defaultTodos);
+
+  const addTodo = (newTodo: TodoModel): void => {
+    setTodos([
+      ...todos,
+      newTodo,
+    ]);
+  }
 
   return (
     <IonPage>
@@ -27,6 +64,10 @@ const Home: React.FC = () => {
 
         <TodoList
           todos={todos}
+        />
+
+        <AddTodoForm
+          addTodo={addTodo}
         />
 
       </IonContent>
